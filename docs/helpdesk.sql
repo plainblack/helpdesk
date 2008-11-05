7874fe5e3e8a33f9e2b9a0357a1045659af0e2ad
Graham Knop <graham@plainblack.com>
2008-11-05 20:50:29 +0000

    separate install from helpdesk

---
+++ docs/helpdesk.sql
@@ -0,0 +1,128 @@
+CREATE TABLE HelpDesk (
+    assetId VARCHAR(22) BINARY NOT NULL,
+    revisionDate BIGINT NOT NULL,
+    viewTemplateId VARCHAR(22) BINARY NOT NULL default 'HELPDESK00000000000001',
+    viewMyTemplateId VARCHAR(22) BINARY NOT NULL default 'HELPDESK00000000000002',
+    viewAllTemplateId VARCHAR(22) BINARY NOT NULL default 'HELPDESK00000000000003',
+    searchTemplateId VARCHAR(22) BINARY NOT NULL default 'HELPDESK00000000000004',
+    manageMetaTemplateId VARCHAR(22) BINARY NOT NULL default 'HELPDESK00000000000005',
+    editMetaFieldTemplateId VARCHAR(22) BINARY NOT NULL default 'HELPDESK00000000000006',
+    notificationTemplateId VARCHAR(22) BINARY NOT NULL default 'HELPDESK00000000000007',
+    editTicketTemplateId VARCHAR(22) BINARY NOT NULL default 'TICKET0000000000000001',
+    viewTicketTemplateId VARCHAR(22) BINARY NOT NULL default 'TICKET0000000000000002',
+    viewTicketRelatedFilesTemplateId VARCHAR(22) BINARY NOT NULL default 'TICKET0000000000000003',
+    viewTicketUserListTemplateId VARCHAR(22) BINARY NOT NULL default 'TICKET0000000000000004',
+    viewTicketCommentsTemplateId VARCHAR(22) BINARY NOT NULL default 'TICKET0000000000000005',
+    viewTicketHistoryTemplateId VARCHAR(22) BINARY NOT NULL default 'TICKET0000000000000006',
+    richEditIdPost VARCHAR(22) BINARY NOT NULL default 'PBrichedit000000000002',
+    approvalWorkflow varchar(22) BINARY NOT NULL default 'pbworkflow000000000003',
+    groupToPost VARCHAR(22) BINARY NOT NULL default '3',
+    groupToChangeStatus VARCHAR(22) BINARY NOT NULL default '3',
+    karmaEnabled TINYINT(4) DEFAULT 0,
+    karmaPerPost INTEGER NOT NULL default 0,
+    karmaToClose INTEGER NOT NULL default 0,
+    defaultKarmaScale INTEGER NOT NULL default 1,
+    sortColumn ENUM ('ticketId ','title','createdBy','creationDate','assignedTo','ticketStatus','lastReplyDate','karmaRank') DEFAULT 'creationDate',
+    sortOrder ENUM('ASC','DESC') DEFAULT 'DESC',
+    subscriptionGroup VARCHAR(255) NOT NULL,
+    mailServer VARCHAR(255) default NULL,
+    mailAccount VARCHAR(255) default NULL,
+    mailPassword VARCHAR(255) default NULL,
+    mailAddress VARCHAR(255) default NULL,
+    mailPrefix VARCHAR(255) default NULL,
+    getMail TINYINT(4) NOT NULL default 0,
+    getMailInterval INTEGER NOT NULL default 300,
+    getMailCronId VARCHAR(22) BINARY default NULL,
+    autoSubscribeToTicket TINYINT(4) NOT NULL default 1,
+    requireSubscriptionForEmailPosting TINYINT(4) NOT NULL default 1,
+    closeTicketsAfter integer not null default 1209600,
+    PRIMARY KEY (assetId,revisionDate)
+);
+
+CREATE TABLE HelpDesk_metaField (
+    fieldId VARCHAR(22) BINARY NOT NULL,
+    assetId VARCHAR(22) BINARY NOT NULL,
+    label VARCHAR(100) DEFAULT NULL,
+    dataType VARCHAR(20) DEFAULT NULL,
+    required TINYINT(4) DEFAULT 0,
+    searchable TINYINT(4) DEFAULT 0,
+    possibleValues TEXT,
+    defaultValues TEXT,
+    hoverHelp TEXT,
+    sequenceNumber INT(5) DEFAULT NULL,
+    PRIMARY KEY  (fieldId)
+);
+
+CREATE TABLE Ticket (
+    assetId VARCHAR(22) BINARY NOT NULL,
+    revisionDate BIGINT NOT NULL,
+    ticketId mediumint not null,
+    ticketStatus VARCHAR(30) NOT NULL default 'pending',
+    assigned tinyint(1) NOT NULL default 0,
+    assignedTo VARCHAR(22) BINARY default NULL,
+    assignedBy VARCHAR(22) BINARY default NULL,
+    dateAssigned BIGINT default NULL,
+    storageId VARCHAR(22) BINARY default NULL,
+    isPrivate tinyint(1) NOT NULL default 0,
+    solutionSummary longtext default NULL,
+    comments longtext default NULL,
+    averageRating float default 0,
+    lastReplyDate BIGINT default NULL,
+    lastReplyBy VARCHAR(22) BINARY default NULL,
+    resolvedBy VARCHAR(22) BINARY default NULL,
+    resolvedDate BIGINT default NULL,
+    karma INTEGER NOT NULL default 0,
+    karmaScale INTEGER NOT NULL default 1,
+    karmaRank FLOAT default NULL,
+    subscriptionGroup VARCHAR(255) NOT NULL,
+    PRIMARY KEY (assetId,revisionDate)
+);
+
+CREATE TABLE Ticket_history (
+    historyId VARCHAR(22) BINARY NOT NULL,
+    userId VARCHAR(22) BINARY NOT NULL,
+    assetId VARCHAR(22) BINARY NOT NULL,
+    actionTaken VARCHAR(255) NOT NULL,
+    dateStamp BIGINT NOT NULL,
+    PRIMARY KEY (historyId)
+);
+
+CREATE TABLE Ticket_metaData (
+    fieldId VARCHAR(22) BINARY NOT NULL,
+    assetId VARCHAR(22) BINARY NOT NULL,
+    value MEDIUMTEXT DEFAULT NULL,
+    PRIMARY KEY  (fieldId, assetId)
+);
+
+CREATE TABLE Ticket_searchIndex (
+    assetId VARCHAR(22) BINARY NOT NULL,
+    parentId VARCHAR(22) BINARY NOT NULL,
+    lineage VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    ticketId MEDIUMINT NOT NULL,
+    creationDate BIGINT NOT NULL, 
+    createdBy VARCHAR(22) NOT NULL,
+    synopsis text NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    keywords VARCHAR(255) NOT NULL,
+    isPrivate tinyint(1) NOT NULL default 0,
+    assignedTo VARCHAR(22) default NULL,
+    assignedBy VARCHAR(22) default NULL,
+    dateAssigned BIGINT default NULL,
+    ticketStatus VARCHAR(30) NOT NULL,
+    solutionSummary longtext default NULL,
+    averageRating float default 0,
+    lastReplyDate BIGINT default NULL,
+    lastReplyBy VARCHAR(22) default NULL,
+    karmaRank FLOAT default NULL,
+    PRIMARY KEY  (assetId)
+);
+
+CREATE TABLE Ticket_collabRef (
+    origAssetId VARCHAR(22) BINARY NOT NULL,
+    mapToAssetId VARCHAR(22) BINARY NOT NULL,
+    PRIMARY KEY  (origAssetId)
+);
+
+INSERT INTO incrementer (incrementerId,nextValue) VALUES ('ticketId',1);
+
---
+++ sbin/installHelpDesk.pl
@@ -0,0 +1,145 @@
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2008 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+$|++; # disable output buffering
+our ($webguiRoot, $configFile, $help, $man);
+
+BEGIN {
+    $webguiRoot = "..";
+    unshift (@INC, $webguiRoot."/lib");
+}
+
+use strict;
+use Pod::Usage;
+use Getopt::Long;
+use WebGUI::Session;
+
+# Get parameters here, including $help
+GetOptions(
+    'configFile=s'  => \$configFile,
+    'help'          => \$help,
+    'man'           => \$man,
+);
+
+pod2usage( verbose => 1 ) if $help;
+pod2usage( verbose => 2 ) if $man;
+pod2usage( msg => "Must specify a config file!" ) unless $configFile;  
+
+my $session = start( $webguiRoot, $configFile );
+
+#Add wobject to config file
+$session->config->set("assets/WebGUI::Asset::Wobject::HelpDesk/category", 'intranet');
+
+#Create row in incrementer table
+
+### Add the Workflow Activities to the config file.
+$session->config->addToArray("workflowActivities/None", "WebGUI::Workflow::Activity::CloseResolvedTickets");
+
+### Create a new workflow for running mail
+my $props = {
+    title        => "Get Help Desk Mail",
+    description  => "Retrieves mail from a POP3 account for the given Help Desk",
+    enabled      => "1",
+    type         => "WebGUI::Asset::Wobject::HelpDesk",
+    mode         => "singleton",
+};
+my $workflow = WebGUI::Workflow->create($session,$props,"hdworkflow000000000001");
+### Add the activity to the workflow
+my $activity = $workflow->addActivity(
+    "WebGUI::Workflow::Activity::GetHelpDeskMail",
+    "hdactivity000000000001"
+);
+$activity->set("title","Get Mail");
+finish($session);
+
+#----------------------------------------------------------------------------
+# Your sub here
+
+#----------------------------------------------------------------------------
+sub start {
+    my $webguiRoot  = shift;
+    my $configFile  = shift;
+    my $session = WebGUI::Session->open($webguiRoot,$configFile);
+    $session->user({userId=>3});
+    
+    ## If your script is adding or changing content you need these lines, otherwise leave them commented
+    #
+    # my $versionTag = WebGUI::VersionTag->getWorking($session);
+    # $versionTag->set({name => 'Name Your Tag'});
+    #
+    ##
+    
+    return $session;
+}
+
+#----------------------------------------------------------------------------
+sub finish {
+    my $session = shift;
+    
+    ## If your script is adding or changing content you need these lines, otherwise leave them commented
+    #
+    # my $versionTag = WebGUI::VersionTag->getWorking($session);
+    # $versionTag->commit;
+    ##
+    
+    $session->var->end;
+    $session->close;
+}
+
+__END__
+
+
+=head1 NAME
+
+utility - A template for WebGUI utility scripts
+
+=head1 SYNOPSIS
+
+ utility --configFile config.conf ...
+
+ utility --help
+
+=head1 DESCRIPTION
+
+This WebGUI utility script helps you...
+
+=head1 ARGUMENTS
+
+=head1 OPTIONS
+
+=over
+
+=item B<--configFile config.conf>
+
+The WebGUI config file to use. Only the file name needs to be specified,
+since it will be looked up inside WebGUI's configuration directory.
+This parameter is required.
+
+=item B<--help>
+
+Shows a short summary and usage
+
+=item B<--man>
+
+Shows this document
+
+=back
+
+=head1 AUTHOR
+
+Copyright 2001-2008 Plain Black Corporation.
+
+=cut
+
+#vim:ft=perl
+
+
+1;
+
