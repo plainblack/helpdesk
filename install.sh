c740a949a3e081c04d0ef60bc5ed7cf92a8c540d
daviddelikat <david-delikat@usa.net>
2009-07-26 12:20:16 -0500

    several fixes for the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 
+echo set source and target:
 helpdesk=/data/helpdesk
 WEBGUI=/data/WebGUI
+echo source: $helpdesk
+echo target: $WEBGUI
 
 # this ran OK ok centos 5
 # it requires wgd for the database part and package part
@@ -9,12 +12,14 @@ WEBGUI=/data/WebGUI
 # note: it may be desirable to create links rather than copy
 
 cd $helpdesk
+echo cd to `pwd`
 
+echo remove existing helpdesk files
 # these commands can be used to un-install the modules & extras for helpdesk
 # remember to incldue the assignments above
-# find sbin/ -type f | xargs -I{} rm -f $WEBGUI/{}
-# find www/ -type f | xargs -I{} rm -f $WEBGUI/{}
-# find lib/ -type f | xargs -I{} rm -f $WEBGUI/{}
+find sbin/ -type f | xargs -I{} rm -f $WEBGUI/{}
+find www/ -type f | xargs -I{} rm -f $WEBGUI/{}
+find lib/ -type f | xargs -I{} rm -f $WEBGUI/{}
 
 # these commands can be used to diff the modules & extras for helpdesk
 # remember to incldue the assignments above
@@ -22,6 +27,7 @@ cd $helpdesk
 # find www/ -type f | xargs -I{} diff -u $helpdesk/{} $WEBGUI/{}
 # find lib/ -type f | xargs -I{} diff -u $helpdesk/{} $WEBGUI/{}
 
+echo link new helpdsk files into WebGUI directory
 find sbin/ -type f | xargs -I{} ln $helpdesk/{} $WEBGUI/{}
 find www/ -type d | xargs -I{} mkdir $WEBGUI/{} 2> /dev/null
 find www/ -type f | xargs -I{} ln $helpdesk/{} $WEBGUI/{}
@@ -29,10 +35,14 @@ find lib/ -type f | xargs -I{} ln $helpdesk/{} $WEBGUI/{}
 # note that it is necessary to re-create the hard links any time you update the git repo...
 
 # be sure to edit the config file:
-cd $WebGUI/sbin
-perl  installHelpDesk.pl --configFile localhost.conf
+cd $WEBGUI/sbin
+echo cd to `pwd`
+echo run helpdesk install
+perl  $helpdesk/sbin/installHelpDesk.pl --configFile localhost.conf
 
-wgd.old db < $helpdesk/helpdesk.sql
+echo create database
+wgd.old db < $helpdesk/docs/helpdesk.sql
 
-wgd package --import $helpdesk/help_desk.wgpkg
+echo install package
+wgd package --import $helpdesk/docs/help_desk.wgpkg
 
