52a3ae6bcf9872dcc90c74e66a1b0a3aaae30798
daviddelikat <david-delikat@usa.net>
2009-08-05 03:22:52 -0500

    added the test code files to intall process

--- install.sh
+++ install.sh
@@ -20,18 +20,21 @@ echo remove existing helpdesk files
 find sbin/ -type f | xargs -I{} rm -f $WEBGUI/{}
 find www/ -type f | xargs -I{} rm -f $WEBGUI/{}
 find lib/ -type f | xargs -I{} rm -f $WEBGUI/{}
+find t/ -type f | xargs -I{} rm -f $WEBGUI/{}
 
 # these commands can be used to diff the modules & extras for helpdesk
 # remember to incldue the assignments above
 # find sbin/ -type f | xargs -I{} diff -u $helpdesk/{} $WEBGUI/{}
 # find www/ -type f | xargs -I{} diff -u $helpdesk/{} $WEBGUI/{}
 # find lib/ -type f | xargs -I{} diff -u $helpdesk/{} $WEBGUI/{}
+# find t/ -type f | xargs -I{} diff -u $helpdesk/{} $WEBGUI/{}
 
 echo link new helpdsk files into WebGUI directory
 find sbin/ -type f | xargs -I{} ln $helpdesk/{} $WEBGUI/{}
 find www/ -type d | xargs -I{} mkdir $WEBGUI/{} 2> /dev/null
 find www/ -type f | xargs -I{} ln $helpdesk/{} $WEBGUI/{}
 find lib/ -type f | xargs -I{} ln $helpdesk/{} $WEBGUI/{}
+find t/ -type f | xargs -I{} ln $helpdesk/{} $WEBGUI/{}
 # note that it is necessary to re-create the hard links any time you update the git repo...
 
 # be sure to edit the config file:
